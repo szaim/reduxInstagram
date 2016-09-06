42f49e41791c9a4cf865a97a362155a0de549c17
szaim <zaimsofia81@gmail.com>
2016-09-06 16:44:01 -0400

    creating the server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
-import 'babel-polyfill';
+import "babel-polyfill";
 import express from 'express';
+import unirest from 'unirest';
+import events from 'events';
+
 
 const HOST = process.env.HOST;
 const PORT = process.env.PORT || 8080;
@@ -9,6 +12,29 @@ console.log(`Server running in ${process.env.NODE_ENV} mode`);
 let app = express();
 
 app.use(express.static(process.env.CLIENT_PATH));
+var access_token = "1633c5b8ca474e92a324f02602b6366a";
+var getFromApi = function() {
+    var emitter = new events.EventEmitter();
+    unirest.get('https://api.instagram.com/v1/locations/search?lat=48.858844&lng=2.294351&access_token=' + access_token)
+           .end(function(response) {
+                if (response.ok) {
+                    emitter.emit('end', response.body);
+                }
+                else {
+                    emitter.emit('error', response.code);
+                }
+            });
+    return emitter;
+};
+
+app.get('/api', function(req, res) {
+    var searchReq = getFromApi()
+   
+    console.log(searchReq);
+
+    res.send({message: 'hello world'})
+
+})
 
 function runServer() {
     return new Promise((resolve, reject) => {
--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,9 @@ module.exports = {
           test: /\.js$/,
           exclude: /(node_modules)/,
           loader: 'babel',
+          query: {
+             presets: ['react', 'es2015', 'stage-1']
+            }
         },
       ]
     },
